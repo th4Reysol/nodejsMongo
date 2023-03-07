84309c4940f9388519213093fc1dcc48e879be2f
th168 <t.h.his.story@gmail.com>
2023-03-07 09:15:55 +0900

    the 2nd

--- models/WordTable.js
+++ models/WordTable.js
@@ -15,4 +15,4 @@ const WordSchema = new mongoose.Schema({
     },
 });
 
-module.exports = mongoose.model("WordTable", WordSchema);
+module.exports = mongoose.model("wordtables", WordSchema);
--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const WordTable = require('./models/WordTable');
+const wordtables = require('./models/WordTable');
 const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 
-app.listen(3000, console.log("動く"));
+app.listen(3000, console.log("Go!!"));
 
 
 
 mongoose.connect("mongodb+srv://thhisstory:FunairiFFF324@cluster0.ourgdrv.mongodb.net/test"
 ).then(()=>
-    console.log("つないだ")
+    console.log("Connecting")
 )
 .catch ((err)=>console.log("error"));
 
-// API発信部分
-app.get("http://localhost:3000/", async(req, res)=>{
+// API Goes
+app.get("/", async(req, res)=>{
     try{
-        const allWords = await WordTable.find();
+        const allWords = await wordtables.find();
         res.status(200).json(allWords);
     }catch(err){
         console.log("error1")
